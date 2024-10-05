83b6d662a87e67c412f16eea58156d7e791c7753
Lucca Pellegrini <luccapellegrini@e.email>
2024-10-05 14:56:04 -0300

    plot.py: permite escolher escala do eixo y

--- utils/plot.py
+++ utils/plot.py
@@ -2,6 +2,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 import sys
+import argparse
 from matplotlib import rcParams
 
 # Set up LaTeX and Palatino font for matplotlib
@@ -12,48 +13,57 @@ def setup_plot_style():
     sns.set_theme(style='whitegrid')
 
 
-def plot_data(data, title, filename):
+def plot_data(data, title, filename, yscale):
     plt.figure(figsize=(10, 6))
     sns.lineplot(
         data=data,
         x='num',
-        y=data['time'] / 1e3,
+        y=data['time'] / 1e9,
         hue='strat',
         marker='',
         linewidth=1,
     )
     plt.title(title)
     plt.xlabel('Array Size (num)')
-    plt.ylabel('Execution Time (ms)')
+    plt.ylabel('Execution Time (s)')  # Update the label to indicate seconds
     plt.legend(title='Pivot Strategy')
-    plt.yscale('log')  # Log scale for better visibility of differences
+    plt.yscale(yscale)  # Set the y-axis scale
     plt.tight_layout()
     plt.savefig(filename, dpi=600, format='svg')
     # plt.show()
 
 
-def plot_all_data(data):
+def plot_all_data(data, yscale):
     vector_types = data['type'].unique()
     for vector_type in vector_types:
         subset = data[data['type'] == vector_type]
-        title = (
-            f'Performance of Quicksort Strategies for {vector_type} Vectors'
-        )
-        filename = f'build/fig/{vector_type}_all_data.svg'
-        plot_data(subset, title, filename)
+        title = f'Performance of Quicksort Strategies for {vector_type} Vectors ({yscale.capitalize()} Scale)'
+        filename = f'build/fig/{vector_type}_all_data_{yscale}.svg'
+        plot_data(subset, title, filename, yscale)
 
 
-def plot_specific_sizes(data, sizes):
+def plot_specific_sizes(data, sizes, yscale):
     filtered_data = data[data['num'].isin(sizes)]
     vector_types = filtered_data['type'].unique()
     for vector_type in vector_types:
         subset = filtered_data[filtered_data['type'] == vector_type]
-        title = f'Performance of Quicksort Strategies for {vector_type} Vectors (Specific Sizes)'
-        filename = f'build/fig/{vector_type}_specific_sizes.svg'
-        plot_data(subset, title, filename)
+        title = f'Performance of Quicksort Strategies for {vector_type} Vectors (Specific Sizes, {yscale.capitalize()} Scale)'
+        filename = f'build/fig/{vector_type}_specific_sizes_{yscale}.svg'
+        plot_data(subset, title, filename, yscale)
 
 
 def main(arguments):
+    parser = argparse.ArgumentParser(
+        description='Plot quicksort performance data.'
+    )
+    parser.add_argument(
+        '--linear', action='store_true', help='Use linear scale for y-axis'
+    )
+    parser.add_argument(
+        '--log', action='store_true', help='Use logarithmic scale for y-axis'
+    )
+    args = parser.parse_args(arguments)
+
     # Load the CSV data
     data = pd.read_csv('build/main.csv')
 
@@ -65,10 +75,17 @@ def main(arguments):
     setup_plot_style()
 
     # Plot data as requested
-    if plot_full:
-        plot_all_data(data)
-    if plot_selected:
-        plot_specific_sizes(data, [100, 1000, 10000])
+    if args.linear or not (args.linear or args.log):
+        if plot_full:
+            plot_all_data(data, 'linear')
+        if plot_selected:
+            plot_specific_sizes(data, [100, 1000, 10000], 'linear')
+
+    if args.log or not (args.linear or args.log):
+        if plot_full:
+            plot_all_data(data, 'log')
+        if plot_selected:
+            plot_specific_sizes(data, [100, 1000, 10000], 'log')
 
 
 if __name__ == '__main__':
